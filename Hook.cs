ff814a4d583c56ecbe37b0785b6af974ee2bc375
factormystic <factormystic@gmail.com>
2015-05-14 21:46:00 -0400

    `private` => `internal` & static where possible

--- Hook.cs
+++ Hook.cs
@@ -11,7 +11,7 @@ namespace FMUtils.KeyboardHook
     public class Hook
     {
         #region PInvoke
-        private enum HookType : int
+        internal enum HookType : int
         {
             WH_JOURNALRECORD = 0,
             WH_JOURNALPLAYBACK = 1,
@@ -39,14 +39,14 @@ namespace FMUtils.KeyboardHook
             public IntPtr extraInfo;
         }
 
-        private int WM_KEYDOWN = 0x100;
-        private int WM_KEYUP = 0x101;
+        internal static int WM_KEYDOWN = 0x100;
+        internal static int WM_KEYUP = 0x101;
 
-        private int WM_SYSKEYDOWN = 0x0104;
-        private int WM_SYSKEYUP = 0x105;
+        internal static int WM_SYSKEYDOWN = 0x0104;
+        internal static int WM_SYSKEYUP = 0x105;
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        internal static extern IntPtr GetModuleHandle(string lpModuleName);
 
         /// <summary>
         /// Installs an application-defined hook procedure into a hook chain.
@@ -57,7 +57,7 @@ namespace FMUtils.KeyboardHook
         /// <param name="dwThreadId">The identifier of the thread with which the hook procedure is to be associated. If this parameter is zero, the hook procedure is associated with all existing threads running in the same desktop as the calling thread.</param>
         /// <returns>If the function succeeds, the return value is the handle to the hook procedure. If the function fails, the return value is NULL. To get extended error information, call GetLastError.</returns>
         [DllImport("user32.dll")]
-        private static extern IntPtr SetWindowsHookEx(HookType idHook, HookProc lpfn, IntPtr hMod, int dwThreadId);
+        internal static extern IntPtr SetWindowsHookEx(HookType idHook, HookProc lpfn, IntPtr hMod, int dwThreadId);
 
         /// <summary>
         /// Removes a hook procedure installed in a hook chain by the SetWindowsHookEx function.
@@ -65,7 +65,7 @@ namespace FMUtils.KeyboardHook
         /// <param name="hhk">A handle to the hook to be removed. This parameter is a hook handle obtained by a previous call to SetWindowsHookEx.</param>
         /// <returns>If the function succeeds, the return value is nonzero. If the function fails, the return value is zero. To get extended error information, call GetLastError.</returns>
         [DllImport("user32.dll")]
-        private static extern int UnhookWindowsHookEx(IntPtr hhk);
+        internal static extern int UnhookWindowsHookEx(IntPtr hhk);
 
         /// <summary>
         /// Passes the hook information to the next hook procedure in the current hook chain. A hook procedure can call this function either before or after processing the hook information.
@@ -76,7 +76,7 @@ namespace FMUtils.KeyboardHook
         /// <param name="lParam">The lParam value passed to the current hook procedure. The meaning of this parameter depends on the type of hook associated with the current hook chain.</param>
         /// <returns>This value is returned by the next hook procedure in the chain. The current hook procedure must also return this value. The meaning of the return value depends on the hook type. For more information, see the descriptions of the individual hook procedures.</returns>
         [DllImport("user32.dll")]
-        private static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
+        internal static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
 
         /// <summary>
         /// An application-defined or library-defined callback function used with the SetWindowsHookEx function. The system calls this function whenever an application calls the GetMessage or PeekMessage function and there is a keyboard message (WM_KEYUP or WM_KEYDOWN) to be processed.
@@ -85,7 +85,7 @@ namespace FMUtils.KeyboardHook
         /// <param name="wParam">The virtual-key code of the key that generated the keystroke message.</param>
         /// <param name="lParam">The repeat count, scan code, extended-key flag, context code, previous key-state flag, and transition-state flag. For more information about the lParam parameter, see Keystroke Message Flags.</param>
         /// <returns>If code is less than zero, the hook procedure must return the value returned by CallNextHookEx. If code is greater than or equal to zero, and the hook procedure did not process the message, it is highly recommended that you call CallNextHookEx and return the value it returns; otherwise bad stuff.</returns>
-        private delegate int HookProc(int code, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
+        internal delegate int HookProc(int code, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
         #endregion
 
         public string Name { get; private set; }
